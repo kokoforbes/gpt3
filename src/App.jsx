cc77416e79843e392a7c637508e7a91d8127862a
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-04-11 10:08:20 +0100

    Imported components into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-// import { Article, Brand, CTA, Feature, Navbar } from "./components";
-import {
-  Blog,
-  Features,
-  Footer,
-  Header,
-  Possibility,
-  WhatGPT3,
-} from "./containers";
+// import Article from "./components/article/Article";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./containers/header/Header";
+import Brand from "./components/brand/Brand";
+import WhatGPT3 from "./containers/whatGPT3/WhatGPT3";
+import Features from "./containers/features/Features";
+import Possibility from "./containers/possibility/Possibility";
+import CTA from "./components/cta/CTA";
+import Blog from "./containers/blog/Blog";
+import Footer from "./containers/footer/Footer";
+import "./App.css";
 
 const App = () => {
   return (
@@ -17,6 +19,13 @@ const App = () => {
         <Navbar />
         <Header />
       </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
